4d859711dc5b4de6e0d8534fd7dfc9c3551cb0e1
Maria Kuchinskaya <mariakuchinskaya16@gmail.com>
2021-11-10 23:36:17 +0300

    Lesson7

---
+++ Lesson7. Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lesson7._Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[,]
+            {
+                {5, 2, 1},
+                {7, 3, 1},
+                {9, 5, 0}
+            };
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                var maxValueInLine = array[i, 0];
+                for (int j = 0; j < array.GetLength(1)-1; j++)
+                {
+                    if (array[i,j] < array[i,j+1])
+                    {
+                        maxValueInLine = array[i, j+1];
+                    }
+                }
+                Console.WriteLine(maxValueInLine);
+            }
+
+        }
+    }
+}
---
+++ Lesson7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lesson7
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var array = new[] {5, 1, 8, 0};
+
+            foreach (var t in array)
+            {
+                Console.WriteLine(t);
+            }
+
+            for (var i = 0; i < array.Length-1; i++)
+            {
+                for (var j = 0; j < array.Length-1; j++)
+                {
+                     if (array[j] < array[j + 1])
+                     {
+                         (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                     }
+                    
+                }
+            }
+
+            foreach (var t in array)
+            {
+                Console.WriteLine(t);
+            }
+        }
+    }
+}
